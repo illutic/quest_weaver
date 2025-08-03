34065536e5e4ffb27fecb87888f32c87fefdea12
george sigalas <georgesg97@gmail.com>
2025-08-03 19:31:14 +0300

    feat: Add Kt lint workflow.

---
+++ scripts/github_cache_utils.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+function is_cached() {
+  local url="$1"
+  local cached_version="$2"
+  local release_info
+  local remote_version
+  local cached_version
+
+  release_info=$(get_release_info "$url")
+  remote_version=$(get_github_release_version "$release_info")
+
+  if [[ "$remote_version" == "$cached_version" ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
