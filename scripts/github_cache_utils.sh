97380d794401a19f2a1d46462159b7a554c2dda1
George Sigalas <57220574+illutic@users.noreply.github.com>
2025-08-10 19:06:36 +0300

    feat: Implement CI/CD workflows for building, testing, and deploying (#5)
    * feat: Update CI to deploy releases
    
    * fixup: Optimizations and Improvements
    
    refactor: remove redundant logging in GitHub API utility scripts
    
    fix: remove unnecessary backslash in file upload command
    
    feat: update logging for missing environment variables to use println
    
    feat: validate version name to ensure it contains numeric characters
    
    feat: clean up CI deployment configuration

--- scripts/github_cache_utils.sh
+++ scripts/github_cache_utils.sh
@@ -3,10 +3,11 @@
 function is_cached() {
   local url="$1"
   local cached_version="$2"
+  local gh_token="$3"
   local release_info
   local remote_version
 
-  release_info=$(get_release_info "$url")
+  release_info=$(get_release_info "$url" "$gh_token")
   remote_version=$(get_github_release_version "$release_info")
 
   if [[ "$remote_version" == "$cached_version" ]]; then
